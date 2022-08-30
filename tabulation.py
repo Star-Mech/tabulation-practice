c41d21162e1ef754623b6dd8caf27d8fd84fb7d1
Star-Mech <HammadStarMech@gmail.com>
2022-08-30 11:45:40 +0500

    Grid Traveler Tabulation Added

--- tabulation.py
+++ tabulation.py
@@ -6,6 +6,29 @@ def fib(n): # returns nth fibo number
     for i in range(2, n+1):
         fibs[i] = fibs[i-1] + fibs[i-2]
 
-    return fibs[6]
+    return fibs[n]
 
-print(fib(100000))
+# print(fib(10))
+
+import numpy as np
+print(np.__version__)
+
+def grid_traveler(n,m): # (rows, columns)
+    # grid = [[0 for _ in range(n)] for _ in range(m)]
+    grid = np.zeros(shape=(n+1,m+1), dtype=np.longdouble)
+    grid[1,1] = 1
+
+    for r in range(n+1):
+        for c in range(m+1):
+            try:
+                grid[r+1,c] += grid[r,c]
+            except IndexError:
+                pass
+            try:
+                grid[r,c+1] += grid[r,c]
+            except IndexError:
+                pass
+
+    print(grid)
+    return grid[-1,-1]
+print(grid_traveler(5,5))
